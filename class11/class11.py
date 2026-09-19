107e50781fcec457768c158390abb6d4cf10327a
youngsong1230@gmail.com <youngsong1230@gmail.com>
2026-09-18 19:12:52 -0700

    class11

--- class11/class11.py
+++ class11/class11.py
@@ -10,7 +10,7 @@ bottom-up dfs:
 
 def dfs(node):
     if not node:
-        return BASE_CASE
+        return base_case
 
     left = dfs(node.left)
     right = dfs(node.right)
@@ -23,7 +23,7 @@ top-down dfs:
 
 def dfs(node, state):
     if not node:
-        return
+        return base_case
 
     new_state = ...
 
@@ -47,7 +47,7 @@ def dfs(node):
 
     print(node.val)
     dfs(node.left)
-    dfs(node.right)
+    dfs(node.r ight)
 
 Result = 1 2 4 5 3
 
@@ -74,4 +74,78 @@ def dfs(node):
     print(node.val)
 
 Result = 4 5 2 3 1
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode | None) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            return max(left, right) + 1
+
+        return dfs(root)
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode | None) -> TreeNode | None:
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
+        
+        def dfs(node, curr_sum):
+            if not node:
+                return False
+
+            curr_sum += node.val
+
+            if not node.left and not node.right:
+                return curr_sum == targetSum
+
+            # left = dfs(node.left, curr_sum)
+
+            # if left:
+            #     return True
+
+            # right = dfs(node.right, curr_sum)
+
+            # if right:
+            #     return True
+
+            return dfs(node.left, curr_sum) or dfs(node.right, curr_sum)
+  
+        return dfs(root, 0)
+
 '''
