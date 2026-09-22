21355253a421d4933eac671e38e7f78d4a94bc30
youngsong1230@gmail.com <youngsong1230@gmail.com>
2026-09-21 19:15:27 -0700

    class12

---
+++ class12/class12.py
@@ -0,0 +1,99 @@
+'''
+1448) Count Good Nodes in Tree
+
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+
+Example 1:
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        self.good_nodes = 0
+
+        def dfs(node, max_val):
+            # 1) base case
+            if not node:
+                return
+            
+            # 2) do some work
+            if node.val >= max_val:
+                max_val = node.val
+                self.good_nodes += 1
+
+            # 3) continue the recursion left and right child
+            dfs(node.left, max_val)
+            dfs(node.right, max_val)
+        
+        dfs(root, root.val)
+        return self.good_nodes
+
+# t.c: o(n)
+# s.c: o(h)
+
+124. Binary Tree Maximum Path Sum
+
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+The path sum of a path is the sum of the node's values in the path.
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+ 
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: TreeNode | None) -> int:
+        self.res = float('-inf')
+
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = max(dfs(node.left), 0)
+            right = max(dfs(node.right), 0)
+
+            curr_path = node.val + left + right
+
+            self.res = max(self.res, curr_path)
+
+            return max(left, right) + node.val
+        
+        dfs(root)
+        return self.res
+
+# t.c: o(n)
+# s.c: o(h)
+'''
