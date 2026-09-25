7395c4f4f81182910220843bcf894643d038205e
Young Song <youngsong1230@gmail.com>
2026-09-25 10:41:25 -0700

    class13 updated

--- class12/class12.py
+++ class12/class12.py
@@ -96,4 +96,42 @@ class Solution:
 
 # t.c: o(n)
 # s.c: o(h)
+
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        if not preorder or not inorder:
+            return None
+
+        # First value in preorder is the root
+        root = TreeNode(preorder[0])
+
+        # Find root inside inorder
+        mid = inorder.index(preorder[0])
+
+        # Build left subtree
+        root.left = self.buildTree(
+            preorder[1:mid + 1],
+            inorder[:mid]
+        )
+
+        # Build right subtree
+        root.right = self.buildTree(
+            preorder[mid + 1:],
+            inorder[mid + 1:]
+        )
+
+        return root
+
 '''
